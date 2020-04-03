99cbfe664415f83957897f051c4504462773409b
rosaj <rosaj@users.noreply.github.com>
2020-04-03 11:43:34 +0200

    Vj_5

---
+++ Vj_5/FlappyBird/Assets/Scripts/ColumnController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<BirdController>() != null)
+        {
+            // If the bird hits the trigger collider in between the columns then
+            // tell the game control that the bird scored
+            GameManager.Instance.BirdScored();
+        }
+    }
+}
---
+++ Vj_5/FlappyBird/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverText;
+    public bool gameOver = false;
+
+    public float scrollSpeed = -3f;
+
+    public Text scoreText;
+    private int score = 0;
+
+    #region Singleton
+
+    public static GameManager Instance { get; private set; } 
+
+    void Awake()
+    {
+        
+        if (Instance == null)
+            Instance = this;
+        else if(Instance != this)
+            Destroy(gameObject);
+
+     }
+
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver && Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            // We need to reset this variable since the GameManager is static
+            // and is not reinitialized
+            gameOver = false;
+        }
+
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+
+
+
+    public void BirdScored()
+    {
+        if (gameOver)
+            return;
+
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
