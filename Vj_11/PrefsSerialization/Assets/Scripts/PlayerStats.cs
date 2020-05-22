9a8b3f7bd727d94d19af8d807e662e71cf0f426b
rosaj <rosaj@users.noreply.github.com>
2020-05-22 11:25:36 +0200

    Vj 11

---
+++ Vj_11/PrefsSerialization/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+[Serializable]
+public class PlayerStats
+{
+    public string name;
+    public float health;
+    public SVector3 position;
+}
---
+++ Vj_11/PrefsSerialization/Assets/Scripts/SVector3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SVector3
+{
+    public float x;
+    public float y;
+    public float z;
+    public static implicit operator Vector3(SVector3 p)
+    {
+        return new Vector3(p.x, p.y, p.z);
+    }
+    public SVector3(Vector3 p)
+    {
+        this.x = p.x;
+        this.y = p.y;
+        this.z = p.z;
+    }
+    public SVector3(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
