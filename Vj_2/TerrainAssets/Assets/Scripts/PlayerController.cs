0bdd816a98975e23a293bd10b4183813bf6fcc34
Robert Šajina <rosaj@unipu.hr>
2021-03-18 17:06:11 +0100

    Vj 2

---
+++ Vj_2/TerrainAssets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+
+    void FixedUpdate()
+    {
+
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+
+        var movement = new Vector3(moveVertical, 0, moveHorizontal*-1);
+
+        transform.position += movement * speed;
+    }
+
+
+}
