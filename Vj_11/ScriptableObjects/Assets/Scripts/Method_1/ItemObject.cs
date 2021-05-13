61c9730c630f12e0487de1fcc0021836d5d105ef
Robert Šajina <rosaj@unipu.hr>
2021-05-13 16:17:47 +0200

    Vj 11

---
+++ Vj_11/ScriptableObjects/Assets/Scripts/Method_1/ItemObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory/Item")]
+public class ItemObject : ScriptableObject
+{
+    public new string name;
+    public GameObject gameObject;
+    public float health;
+    public float strength;
+
+
+    public string Info()
+    {
+        return string.Format("{0} - health: {1}, strength: {2}", name, health, strength);
+    }
+    
+}
---
+++ Vj_11/ScriptableObjects/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public PlayerStats stats;
+
+    // Method 1
+    public void PickedUp(ItemObject item)
+    {
+        stats.health += item.health;
+        stats.strength += item.strength;
+    }
+
+    // Method 2
+    public void PickedUp(DBPickUpItem item)
+    {
+        stats.health += item.health;
+        stats.strength += item.strength;
+    }
+}
